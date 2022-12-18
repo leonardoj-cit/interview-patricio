073418cfbdf5968ac1cae9e5b80c1c7b3f27851b
patriciocordeiro <paponcio6@gmail.com>
2022-12-18 08:10:38 -0300

    generating random string for Authorization

--- src/app/core/interceptors/auth-interceptor.ts
+++ src/app/core/interceptors/auth-interceptor.ts
@@ -4,11 +4,16 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
-    const authToken = Date.now().toString();
+    const authToken = this.genAuthToken();
     const authReq = req.clone({
       headers: req.headers.set('Authorization', authToken),
     });
 
     return next.handle(authReq);
+  }
+
+  private genAuthToken() {
+    const sixteenDigitsNumber = Math.random().toString().substring(2, 18);
+    return sixteenDigitsNumber;
   }
 }
